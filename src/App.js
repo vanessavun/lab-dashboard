2e6ddeea8f13e90665efc843209e1355a0610bf3
Vanessa Vun <vanessavun@gmail.com>
2022-08-17 23:43:23 -0700

    Created mock main dashboard page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, { userEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Genlab, Samples, Calendar, Customers, Employees, Line, Stacked, Pyramid, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping } from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const { activeMenu } = useStateContext();
+    return (
+    <div>
+        <BrowserRouter>
+            <div className='flex relative dark:bg-main-dark-bg'>
+                <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+                    <TooltipComponent content='Settings' position='Top'>
+                        <button type='button'
+                            className='text-3xl p-3
+                            hover:drop-shadow-xl
+                            hover:bg-light-gray
+                            text-white'
+                            style={{ background: 'blue',
+                            borderRadius: '50%' }}>
+                            <FiSettings />
+                        </button>
+                    </TooltipComponent>
+                </div>
+                {activeMenu ? (
+                    <div className='w-72 fixed sidebar
+                    dark:bg-secondary-dark-bg 
+                    bg-white'>
+                        <Sidebar />
+                    </div>
+                ) : (
+                    <div className='w-0 dark:bg-secondary-dark-bg'>
+                        <Sidebar />
+                    </div>
+                )}
+                <div className={
+                    `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+                }>
+                    <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                    <Navbar />
+                    </div>
+
+                <div>
+                    <Routes>
+                        {/* Dashboard */}
+                        <Route path='/' element={<Genlab />} />
+                        <Route path='/genlab' element={<Genlab />} />
+
+                        {/* Pages */}
+                        <Route path='/samples' element={<Samples />} />
+                        <Route path='/employees' element={<Employees />} />
+                        <Route path='/customers' element={<Customers />} />
+                        {/* <Route path='/inventory' element='Inventory' /> */}
+
+                        {/* Apps */}
+                        <Route path='/kanban' element={<Kanban />} />
+                        <Route path='/calendar' element={<Calendar />} />
+                        <Route path='/color-picker' element={<ColorPicker />} />
+
+                        {/* Charts */}
+                        {/* <Route path='/pending' element='Pending' />
+                        <Route path='/extraction' element='Extraction' />
+                        <Route path='/accession' element='Accession' />
+                        <Route path='/tat' element='TAT' />*/}
+                        <Route path="/line" element={<Line />} />
+                        <Route path="/area" element={<Area />} />
+                        <Route path="/bar" element={<Bar />}/>
+                        <Route path="/pie" element={<Pie />}/>
+                        <Route path="/financial" element={<Financial />} />
+                        <Route path="/color-mapping" element={<ColorMapping />} />
+                        <Route path="/pyramid" element={<Pyramid />} />
+                        <Route path="/stacked" element={<Stacked />}/>
+                    </Routes>
+                </div>
+                </div>
+            </div>
+        </BrowserRouter>
     </div>
-  );
+    )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+      fontFamily: {
+        display: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      extend: {
+        fontSize: {
+          14: '14px',
+        },
+        backgroundColor: {
+          'main-bg': '#FAFBFB',
+          'main-dark-bg': '#20232A',
+          'secondary-dark-bg': '#33373E',
+          'light-gray': '#F7F7F7',
+          'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        },
+        borderWidth: {
+          1: '1px',
+        },
+        borderColor: {
+          color: 'rgba(0, 0, 0, 0.1)',
+        },
+        width: {
+          400: '400px',
+          760: '760px',
+          780: '780px',
+          800: '800px',
+          1000: '1000px',
+          1200: '1200px',
+          1400: '1400px',
+        },
+        height: {
+          80: '80px',
+        },
+        minHeight: {
+          590: '590px',
+        },
+        backgroundImage: {
+          'hero-pattern':
+            "url('https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80')",
+        },
+      },
+    },
+    plugins: [],
+  };
